24ae7716ace9f641b711bf3020465888871c7b3d
Peter Phanouvong <peterphanouvong@gmail.com>
2024-08-09 11:26:45 +1000

    drill

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,14 @@ import { KindeProvider } from "@kinde-oss/kinde-auth-react";
 import App from "./App.jsx";
 import "./index.css";
 
-// const handleRedirectCallback = (user, appState) => {
-//   console.log({ user, appState });
-//   if (appState?.redirectTo) {
-//     window.location.href = appState.redirectTo;
-//   } else {
-//     window.location.href = "/";
-//   }
-// };
+const handleRedirectCallback = (user, appState) => {
+  console.log({ user, appState });
+  // if (appState?.redirectTo) {
+  //   window.location.href = appState.redirectTo;
+  // } else {
+  //   window.location.href = "/";
+  // }
+};
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -20,7 +20,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       domain={import.meta.env.VITE_KINDE_DOMAIN}
       logoutUri={window.location.origin}
       redirectUri={window.location.origin}
-      // onRedirectCallback={handleRedirectCallback}
+      onRedirectCallback={handleRedirectCallback}
     >
       <App />
     </KindeProvider>
