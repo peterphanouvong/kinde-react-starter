4bec91351c56b67e57ec8ef48d8af82c2c816666
Peter Phanouvong <peterphanouvong@gmail.com>
2024-08-09 11:23:16 +1000

    provider

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,23 @@ import { KindeProvider } from "@kinde-oss/kinde-auth-react";
 import App from "./App.jsx";
 import "./index.css";
 
+const handleRedirectCallback = (user, appState) => {
+  console.log({ user, appState });
+  if (appState?.redirectTo) {
+    window.location.href = appState.redirectTo;
+  } else {
+    window.location.href = "/";
+  }
+};
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <KindeProvider
       clientId={import.meta.env.VITE_KINDE_CLIENT_ID}
       domain={import.meta.env.VITE_KINDE_DOMAIN}
-      logoutUri={import.meta.env.VITE_KINDE_LOGOUT_URL}
-      redirectUri={import.meta.env.VITE_KINDE_REDIRECT_URL}
+      logoutUri={window.location.origin}
+      redirectUri={window.location.origin}
+      onRedirectCallback={handleRedirectCallback}
     >
       <App />
     </KindeProvider>
