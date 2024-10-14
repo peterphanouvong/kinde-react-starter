43d698f7f1f1bf8fd85101603a4c3b9503b0c5c7
Peter Phanouvong <peterphanouvong@gmail.com>
2024-10-15 09:44:12 +1100

    remove redirect callback

--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,16 @@ import { KindeProvider } from "@kinde-oss/kinde-auth-react";
 import App from "./App.jsx";
 import "./index.css";
 
-const handleRedirectCallback = (user, appState) => {
-  console.log({ user, appState });
-  if (appState?.redirectTo) {
-    console.log("Redirecting to", appState.redirectTo);
-    window.location = appState.redirectTo;
-  } else {
-    console.log("Redirecting to /");
-    window.location = "/";
-  }
-};
+// const handleRedirectCallback = (user, appState) => {
+//   console.log({ user, appState });
+//   if (appState?.redirectTo) {
+//     console.log("Redirecting to", appState.redirectTo);
+//     window.location = appState.redirectTo;
+//   } else {
+//     console.log("Redirecting to /");
+//     window.location = "/";
+//   }
+// };
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -22,7 +22,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       domain={import.meta.env.VITE_KINDE_DOMAIN}
       logoutUri={window.location.origin}
       redirectUri={window.location.origin}
-      onRedirectCallback={handleRedirectCallback}
+      // onRedirectCallback={handleRedirectCallback}
     >
       <App />
     </KindeProvider>
